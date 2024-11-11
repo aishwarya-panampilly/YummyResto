0b324ca0d2dfe1ac99d397f9c58c288a26a0b75c
aishwarya-panampilly <aishwarya.panampilly@gmail.com>
2024-11-11 22:40:31 +0530

    finally committing

---
+++ src/main/java/com/aishpam/yummyresto/YummyRestoApplication.java
@@ -0,0 +1,13 @@
+package com.aishpam.yummyresto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummyRestoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YummyRestoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aishpam/yummyresto/YummyRestoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aishpam.yummyresto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyRestoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
